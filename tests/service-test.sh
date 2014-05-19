6ac0ebf414268a3d014692bb5e67b2167d81de2b
cesine <cesine@yahoo.com>
2014-05-19 12:53:44 +0400

    running tests when we turn on the audio service

--- tests/service-test.sh
+++ tests/service-test.sh
@@ -26,31 +26,40 @@ TESTSFAILEDSTRING="";
 TESTPASSED=0;
 TESTCOUNTEXPECTED=4;
 
+# Production server is using http behind nginx
+SERVER="https://localhost:3184";
+if [ "$NODE_DEPLOY_TARGET" == "production" ]; then
+  SERVER="http://localhost:3184";
+  echo "Using $SERVER"
+else
+  echo "Using $SERVER"
+fi
+
 echo "It should accept short audio"
 TESTCOUNT=$[TESTCOUNT + 1]
-curl -k -F files[]=@sphinx4files/lattice/10001-90210-01803.wav -F token=mytokengoeshere -F username=testingupload -F dbname=testingupload-firstcorpus https://localhost:3184/upload/extract/utterances ||{
+curl -k -F files[]=@sphinx4files/lattice/10001-90210-01803.wav -F token=mytokengoeshere -F username=testingupload -F dbname=testingupload-firstcorpus $SERVER/upload/extract/utterances ||{
 	TESTFAILED=$[TESTFAILED + 1]
   TESTSFAILEDSTRING="$TESTSFAILEDSTRING : It should accept short audio"
 };
 
 echo "It should accept amr audio from androids"
 TESTCOUNT=$[TESTCOUNT + 1]
-mv 13157700051593730_2011-09-11_15.41_1315770072221_.mp3 13157700051593730_2011-09-11_15.41_1315770072221_.amr
-curl -k -F files[]=@13157700051593730_2011-09-11_15.41_1315770072221_.amr -F token=mytokengoeshere -F username=testingupload -F dbname=testingupload-firstcorpus https://localhost:3184/upload/extract/utterances ||{
+cp 13157700051593730_2011-09-11_15.41_1315770072221_.mp3 13157700051593730_2011-09-11_15.41_1315770072221_.amr
+curl -k -F files[]=@13157700051593730_2011-09-11_15.41_1315770072221_.amr -F token=mytokengoeshere -F username=testingupload -F dbname=testingupload-firstcorpus $SERVER/upload/extract/utterances ||{
 	TESTFAILED=$[TESTFAILED + 1]
   TESTSFAILEDSTRING="$TESTSFAILEDSTRING : It should accept amr audio from androids"
 };
 
 echo "It should accept multiple files"
 TESTCOUNT=$[TESTCOUNT + 1]
-curl -k -F files=@$HOME/Documents/georgian/phrases/alo.mp3 -F files=@$HOME/Documents/georgian/phrases/ara.mp3 -F token=mytokengoeshere -F username=testingupload -F dbname=testingupload-firstcorpus https://localhost:3184/upload/extract/utterances ||{
+curl -k -F files=@$HOME/Documents/georgian/phrases/alo.mp3 -F files=@$HOME/Documents/georgian/phrases/ara.mp3 -F token=mytokengoeshere -F username=testingupload -F dbname=testingupload-firstcorpus $SERVER/upload/extract/utterances ||{
 	TESTFAILED=$[TESTFAILED + 1]
   TESTSFAILEDSTRING="$TESTSFAILEDSTRING : It should accept multiple files"
 };
 
 echo "It should accept long movies"
 TESTCOUNT=$[TESTCOUNT + 1]
-curl -k -F files[]=@$HOME/Documents/georgian/elicitation_sessions/ჩემი\ ცოლის\ დაქალის\ ქორწილი\ \[HD\].mp4 -F token=mytokengoeshere -F username=testingupload -F dbname=testingupload-firstcorpus https://localhost:3184/upload/extract/utterances ||{
+curl -k -F files[]=@$HOME/Documents/georgian/elicitation_sessions/ჩემი\ ცოლის\ დაქალის\ ქორწილი\ \[HD\].mp4 -F token=mytokengoeshere -F username=testingupload -F dbname=testingupload-firstcorpus $SERVER/upload/extract/utterances ||{
 	TESTFAILED=$[TESTFAILED + 1]
   TESTSFAILEDSTRING="$TESTSFAILEDSTRING : It should accept long movies"
 };
