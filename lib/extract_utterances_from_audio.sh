bb9849f8e20f07148d8b6a407b5972502d18dd77
cesine <cesine@yahoo.com>
2015-08-17 14:55:49 -0400

    workaround march 29-aug 17 2015

--- lib/extract_utterances_from_audio.sh
+++ lib/extract_utterances_from_audio.sh
@@ -2,15 +2,13 @@
 
 # Running Praat on Mac via LaunchDeamon is failing https://discuss.atom.io/t/how-to-execute-node-js-child-process-from-package/4880/2
 # "textGridInfo": "Command failed: /bin/sh -c /Applications/Praat.app/Contents/MacOS/Praat praat-script-syllable-nuclei-v2file.praat -20 4 0.4 0.1 no \"Instructions_TDFM\"   \"Instructions_TDFM.mp3\"\n",
-# echo $PATH
-# echo ""
-# echo "Running command: "
-# echo "1 $1 2 $2 3 $3 4 $4 5 $5 6 $6 7 $7 8 $8 9 $9"
-# echo ""
-# /Applications/Praat.app/Contents/MacOS/Praat $1 $2 $3 $4 $5 $6 $7 $8 $9 
 
-# Workaround with placeholder TextGrid until we fix  https://github.com/OpenSourceFieldlinguistics/FieldDB/issues/1932
-
-cat lib/praat_segfault_workaround.TextGrid > "$7/$8.TextGrid"
-# wait 1
-cat lib/praat_segfault_workaround.json
+PATH=$PATH:/opt/X11/bin
+#echo "Running command: "
+#echo "1 $1 2 $2 3 $3 4 $4 5 $5 6 $6 7 $7 8 $8 9 $9"
+# echo ""
+#/Applications/Praat.app/Contents/MacOS/Praat $1 $2 $3 $4 $5 $6 $7 $8 $9 
+#echo " saving to $8.TextGrid"
+cat /Users/fielddb/fielddbhome/AudioWebService/lib/praat_segfault_workaround.TextGrid > "$7/$8.TextGrid"
+wait 1
+cat /Users/fielddb/fielddbhome/AudioWebService/lib/praat_segfault_workaround.json
