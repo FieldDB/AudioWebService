06f6818970c07bd7f9f6aee8c3de504e7af3402d
FieldDB <info@fieldlinguist.com>
2020-07-16 17:28:25 -0400

    use uploadDir depending on deploy target

--- middleware/multipartParser.js
+++ middleware/multipartParser.js
@@ -1,9 +1,12 @@
 var formidable = require('formidable');
+var uploadDir = process.env.NODE_DEPLOY_TARGET === "production" ? '/data/tmp' : '/tmp';
 
+console.log('Using upload dir of ' + uploadDir);
 function multipartMiddleware(req, res, next) {
   new formidable.IncomingForm({
     multiples: true,
     maxFileSize: 1610612736,
+    uploadDir: uploadDir,
   }).parse(req, (err, fields, files) => {
     if (err) {
       console.error(new Date() + 'Error', err)
